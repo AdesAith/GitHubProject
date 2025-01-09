0b542f1f0846c106ba11c351096217c5b596a608
AdesAith <yoela.aithnard@stud.uni-heidelberg.de>
2025-01-09 10:13:53 +0100

    Zusammenführung

--- GitHubProject.py
+++ GitHubProject.py
@@ -6,3 +6,33 @@
 #    i = i+1
 
 # test
+# Ersetze alle Zahlen, die durch 3 teilbar sind, durch "digital"
+
+#n = 1
+#while (n<=100):
+#    if(n%3==0):
+#        print("digital")
+#    else: print(nondigital)
+#    n = n+1
+
+#m = 1
+#while(m<=100):
+#    if(m % 5==0):
+#        print("history")
+#    else:
+#        print(m)
+#    m=m+1
+
+q = 0
+
+while q < 100:
+    q+= 1
+    if(q%15 == 0):
+        print("digital history")
+    elif (q%5 ==0):
+        print("history")
+    elif (q%3 == 0):
+        print("digital")
+    else:
+        print(q)
+
